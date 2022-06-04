d448a7bbb03a60e9547069bae75e7130b81e3cd7
Erick Sena <ericksena621@gmail.com>
2022-06-04 15:11:01 -0300

    feat: show menu added

--- main.cpp
+++ main.cpp
@@ -4,11 +4,32 @@
 
 using namespace std;
 
+bool showMenu();
+
 int main()
 {
-
-    TicTacToe game = TicTacToe();
-    game.start();
+    bool willPlay = showMenu();
+    if (willPlay)
+    {
+        TicTacToe game = TicTacToe();
+        game.start();
+    }
 
     return 0;
+}
+
+bool showMenu()
+{
+    int answer;
+    do
+    {
+        system("cls");
+        cout << "\tTicTacToe - Made by Erick Sena" << endl
+             << endl;
+        cout << "1-Play" << endl;
+        cout << "2-Exit" << endl;
+
+        answer = input();
+    } while (answer <= 0 || answer > 2);
+    return answer == 1;
 }
